0b28b76a216c7bcda7ba15289f38471ad617ed77
Rafael Victor Ruwer Araujo <rafaelruweraraujo@gmail.com>
2020-09-09 14:55:15 -0300

    Fix typo

--- Sources/AirtableKit/Extensions.swift
+++ Sources/AirtableKit/Extensions.swift
@@ -11,7 +11,7 @@ extension Array {
     /// Reference: https://www.hackingwithswift.com/example-code/language/how-to-split-an-array-into-chunks
     ///
     /// - Parameter chunkSize: Maximum size of each chunk.
-    /// - Returns: The same elements of the receiver, gouped into chunks of at most `chunkSize` elements.
+    /// - Returns: The same elements of the receiver, grouped into chunks of at most `chunkSize` elements.
     func chunked(by chunkSize: Int) -> [[Element]] {
         stride(from: startIndex, to: endIndex, by: chunkSize).map {
             Array(self[$0 ..< Swift.min(index($0, offsetBy: chunkSize), endIndex)])
